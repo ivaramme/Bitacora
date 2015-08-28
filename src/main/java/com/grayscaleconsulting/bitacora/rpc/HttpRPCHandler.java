bba78b339ac98256d000154eefb63c66a4e2f926
Ivan Ramirez <ivan.ramirez@gmail.com>
2015-08-28 09:40:10 -0400

    Fix response types for HTTP Endpoints

--- src/main/java/com/grayscaleconsulting/bitacora/rpc/HttpRPCHandler.java
+++ src/main/java/com/grayscaleconsulting/bitacora/rpc/HttpRPCHandler.java
@@ -7,6 +7,7 @@ import com.grayscaleconsulting.bitacora.data.metadata.KeyValue;
 
 import com.grayscaleconsulting.bitacora.metrics.Metrics;
 import com.yammer.metrics.core.Counter;
+import org.apache.http.HttpResponse;
 import org.mortbay.jetty.handler.AbstractHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,6 +26,9 @@ import java.io.IOException;
 public class HttpRPCHandler extends AbstractHandler {
     protected static final Logger logger = LoggerFactory.getLogger(HttpRPCHandler.class);
 
+    public static final String RPC_ENDPOINT         = "/rpc";
+    public static final String RPC_CLUSTER_ENDPOINT = "/rpc/cluster";
+
     private final Counter requests = Metrics.getDefault().newCounter(HttpRPCHandler.class, "rpc-http-requests");
     private final Counter clusterRequests = Metrics.getDefault().newCounter(HttpRPCHandler.class, "rpc-http-cluster-requests");
     private final Counter publicRequests = Metrics.getDefault().newCounter(HttpRPCHandler.class, "rpc-http-public-requests");
@@ -39,14 +43,14 @@ public class HttpRPCHandler extends AbstractHandler {
     
     @Override
     public void handle(String endpoint, HttpServletRequest req, HttpServletResponse resp, int i) throws IOException, ServletException {
-        if(endpoint.startsWith(RPCUtils.RPC_ENDPOINT) || endpoint.startsWith(RPCUtils.RPC_CLUSTER_ENDPOINT)) {
+        if(endpoint.startsWith(RPC_ENDPOINT) || endpoint.startsWith(RPC_CLUSTER_ENDPOINT)) {
             logger.info("New request for endpoint: " + endpoint);
             requests.inc();
             
             try {
                 if(null == dataManager) {
                     logger.error("RPC service is not configured correctly");
-                    RPCUtils.sendServerErrorResponse(resp);
+                    sendErrorResponse(resp, HttpServletResponse.SC_SERVICE_UNAVAILABLE);
                     return;
                 }
 
@@ -56,19 +60,19 @@ public class HttpRPCHandler extends AbstractHandler {
                     String key = req.getParameter("key");
                     if (null == key || key.isEmpty() || key.trim().isEmpty()) {
                         logger.info("Missing request information");
-                        RPCUtils.sendMissingData(resp);
+                        sendErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST);
                         resp.flushBuffer();
                         return;
                     }
 
                     // Forward request to cluster in case this is a normal RPC request. 
                     // If the request came from the cluster rpc endpoint, do not forward
-                    boolean forwardRPCRequest = endpoint.equals(RPCUtils.RPC_ENDPOINT);
+                    boolean forwardRPCRequest = endpoint.equals(RPC_ENDPOINT);
                     KeyValue keyValue = dataManager.getRaw(key, forwardRPCRequest);
                     if (null == keyValue) {
                         logger.info(key + " was not found, returning null value");
                         clusterRequests.inc();
-                        RPCUtils.sendNotFoundResponse(key, resp);
+                        sendErrorResponse(resp, HttpServletResponse.SC_NOT_FOUND, "{ \"message\": \"Key "+ key +" not found\"} ");
                     } else {
                         publicRequests.inc(); // public requests will always forward
                         sendResponse(keyValue, resp, forwardRPCRequest);
@@ -93,7 +97,7 @@ public class HttpRPCHandler extends AbstractHandler {
                     }
                 } else {
                     logger.info("Unknown method for endpoint: " + endpoint + " -> " + req.getMethod());
-                    RPCUtils.sendInvalidEndpoint(endpoint, resp);
+                    sendErrorResponse(resp, HttpServletResponse.SC_NOT_IMPLEMENTED);
                 }
             } catch (IOException ioe) {
                 logger.error("Error sending response to request. ");
@@ -101,7 +105,7 @@ public class HttpRPCHandler extends AbstractHandler {
             }
         }else {
             logger.info("Invalid endpoint: " + endpoint);
-            RPCUtils.sendInvalidEndpoint(endpoint, resp);
+            sendErrorResponse(resp, HttpServletResponse.SC_NOT_IMPLEMENTED);
         }
         resp.flushBuffer();
     }
@@ -109,8 +113,18 @@ public class HttpRPCHandler extends AbstractHandler {
     private void sendResponse(KeyValue value, HttpServletResponse resp, boolean forwardRequest) throws IOException {
         logger.info(value + " was found " + (!forwardRequest ? "in local cache returning to fellow node" : ""));
 
-        resp.setContentType("application/javascript");
+        resp.setContentType("application/json");
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().println(jsonParser.toJson(value, KeyValue.class));
+    }
+    
+    private void sendErrorResponse(HttpServletResponse resp, int code) throws IOException {
+        sendErrorResponse(resp, code, "{ \"message\": \"Error: "+ code +"\"} ");
+    }
+
+    private void sendErrorResponse(HttpServletResponse resp, int code, String message) throws IOException {
+        resp.setContentType("application/json");
+        resp.setStatus(code);
+        resp.getWriter().print(message);
     }
 }
